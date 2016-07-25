6bd0c34887c390727b7db5725dda4c49d156dc06
yavorska <yavorsko@tcd.ie>
2016-07-25 19:53:44 +0100

    Update consistency.of.phenotypes-methods.R

--- R/consistency.of.phenotypes-methods.R
+++ R/consistency.of.phenotypes-methods.R
@@ -38,31 +38,31 @@ setMethod(f = "consistency.of.phenotypes",
 
             if(type == "box"){
 
-              plot(ggplot(meltedDF, aes(x = variable, y = value, fill = variable)) +
+              .plot <- ggplot(meltedDF, aes(x = variable, y = value, fill = variable)) +
                 geom_hline(yintercept = 0, alpha = 0.3, linetype = "dashed", color = "red") +
                 geom_boxplot(alpha = 0.4) +
                 xlab(paste(geneName, "shRNAs")) + theme(axis.text.x = element_blank()) +
                 ylab("FC") +
                 ggtitle(paste("Consistency of shRNAs across", entityName, "cell lines")) +
                 scale_fill_discrete(name = "shRNAs") +
-                theme)
+                theme
 
-              output.df <- data.frame(shRNA = geneNames.number, sequence = geneNames)
-              print.data.frame(output.df)
+                output.df <- data.frame(shRNA = geneNames.number, sequence = geneNames)
+                return(list(shRNAs = output.df, Plot = .plot))
 
             } else if (type == "line") {
 
-              plot(ggplot(meltedDF, aes(x = variable, y = value)) +
+              .plot <- ggplot(meltedDF, aes(x = variable, y = value)) +
                 geom_line(alpha = 0.7, aes(colour = CellLine, group = CellLine)) +
                 stat_summary(fun.y = median, geom = "line", colour = "red", size = 1,
                              aes(group = 1)) +
                 xlab(paste(geneName, "shRNAs")) + theme(axis.text.x = element_text(angle = 90)) +
                 ylab("FC") +
                 ggtitle(paste("Consistency of shRNAs across", entityName, "cell lines")) +
-                theme)
+                theme
 
-              output.df <- data.frame(shRNA = geneNames.number, sequence = geneNames)
-              print.data.frame(output.df)
+                output.df <- data.frame(shRNA = geneNames.number, sequence = geneNames)
+                return(list(shRNAs = output.df, Plot = .plot))
 
             } else {
               cat("The type of plot chosen must be one of : box or line.")
