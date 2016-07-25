be8d7a5c26035f038036f2116316c87813f3be4f
yavorska <yavorsko@tcd.ie>
2016-07-25 17:16:57 +0100

    Update names of shRNAs

--- R/consistency.of.phenotypes-methods.R
+++ R/consistency.of.phenotypes-methods.R
@@ -3,6 +3,8 @@ setMethod(f = "consistency.of.phenotypes",
           definition = function(object, entityName, geneName, type = "box"){
 
             geneIndex <- which(object@genes == geneName)
+            geneNames.number <- paste(geneName, 1:length(geneIndex), sep = "_")
+            geneNames <- paste(geneName, object@sequences[geneIndex], sep = "_")
             ngenes <- length(geneIndex)
 
             valuesDF <- object@values
@@ -25,7 +27,7 @@ setMethod(f = "consistency.of.phenotypes",
               stop
             }
 
-            colnames(subsetDF) <- paste(geneName, object@sequences[which(object@genes == geneName)], sep = "_")
+            colnames(subsetDF) <- geneNames.number
 
             subsetDF <- cbind(CellLine = rownames(subsetDF), subsetDF)
 
@@ -36,25 +38,31 @@ setMethod(f = "consistency.of.phenotypes",
 
             if(type == "box"){
 
-              ggplot(meltedDF, aes(x = variable, y = value, fill = variable)) +
+              plot(ggplot(meltedDF, aes(x = variable, y = value, fill = variable)) +
                 geom_hline(yintercept = 0, alpha = 0.3, linetype = "dashed", color = "red") +
                 geom_boxplot(alpha = 0.4) +
                 xlab(paste(geneName, "shRNAs")) + theme(axis.text.x = element_blank()) +
                 ylab("FC") +
                 ggtitle(paste("Consistency of shRNAs across", entityName, "cell lines")) +
                 scale_fill_discrete(name = "shRNAs") +
-                theme
+                theme)
+
+              output.df <- data.frame(shRNA = geneNames.number, sequence = geneNames)
+              print.data.frame(output.df)
 
             } else if (type == "line") {
 
-              ggplot(meltedDF, aes(x = variable, y = value)) +
+              plot(ggplot(meltedDF, aes(x = variable, y = value)) +
                 geom_line(alpha = 0.7, aes(colour = CellLine, group = CellLine)) +
                 stat_summary(fun.y = median, geom = "line", colour = "red", size = 1,
                              aes(group = 1)) +
                 xlab(paste(geneName, "shRNAs")) + theme(axis.text.x = element_text(angle = 90)) +
                 ylab("FC") +
                 ggtitle(paste("Consistency of shRNAs across", entityName, "cell lines")) +
-                theme
+                theme)
+
+              output.df <- data.frame(shRNA = geneNames.number, sequence = geneNames)
+              print.data.frame(output.df)
 
             } else {
               cat("The type of plot chosen must be one of : box or line.")
