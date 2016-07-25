ce077219cff65ca82cd332b7369e160d3379e4fd
yavorska <yavorsko@tcd.ie>
2016-07-25 17:03:56 +0100

    Fixed median and truncated title

--- R/variation.within.cell.line-methods.R
+++ R/variation.within.cell.line-methods.R
@@ -8,36 +8,40 @@ setMethod(f = "variation.within.cell.line",
             cancerIndex <- grep(cancerID, object@cancers, ignore.case = TRUE)
 
             valuesDF <- object@values
+            newDF <- data.frame(Gene = object@genes, Value = valuesDF[,cancerIndex])
+            newDF <- newDF[complete.cases(newDF),]
+            newDF$Rank <- rank(newDF$Value)
+            geneIndex <- which(newDF$Gene == geneName)
+            ngenes <- length(geneIndex)
 
-            sortedData <- data.frame(lon = rank(valuesDF[,cancerIndex])[geneIndex],
-                                     lat = valuesDF[geneIndex,cancerIndex])
-
-            valuesDF <- object@values[,cancerIndex]
-            valuesDF <- data.frame(Value = valuesDF, Rank = rank(valuesDF))
-            valuesDF <- valuesDF[complete.cases(valuesDF),]
-
+            sortedData <- data.frame(lon = newDF$Rank[geneIndex],
+                                     lat = newDF$Value[geneIndex])
 
             if(type == "all"){
-              ggplot(valuesDF, aes(x = Rank,
-                                   y = Value)) +
+              ggplot(newDF, aes(x = Rank,
+                                y = Value)) +
 
                 geom_point(colour = "grey63", size = 0.75) +
                 geom_point(data = sortedData, alpha = 0.9, aes(x = lon, y = lat), colour="dodgerblue4", size = 2) +
                 geom_segment(data = sortedData, aes(x = lon, xend = lon, y = lat, yend = 0), colour = "dodgerblue4") +
-                ggtitle(paste(geneName, "shRNA ranks within the ", object@cancers[cancerIndex], "cell line")) +
+                ggtitle(paste(geneName, "shRNA ranks within", object@cancers[cancerIndex])) +
                 xlab("Ranked shRNAs") +
                 ylab("shRNA FC Score") +
                 theme
 
             } else if (type == "median") {
 
-              ggplot(valuesDF, aes(x = Rank,
-                                   y = Value)) +
+              x.val <- round(median(sortedData$lon))
+              y.val <- newDF$Value[which(newDF$Rank == x.val)]
 
+              ggplot(newDF, aes(x = Rank,
+                                y = Value)) +
                 geom_point(colour = "grey63", size = 0.75) +
-                geom_point(alpha = 0.9, aes(x = median(lon), y = median(lat)), data = sortedData, colour = ifelse(median(sortedData$lat) > 0, "green", "red"), size = 2) +
-                geom_segment(aes(x = median(lon), xend = median(lon), y = median(lat), yend = 0), data = sortedData, colour = ifelse(median(sortedData$lat) > 0, "green", "red")) +
-                ggtitle(paste(geneName, "shRNA median rank within the", object@cancers[cancerIndex], "cell line")) +
+                geom_point(alpha = 0.9, aes(x = x.val, y = y.val),
+                           data = NULL, colour = ifelse(y.val > 0, "green", "red"), size = 2) +
+                geom_segment(aes(x = x.val, xend = x.val, y = y.val, yend = 0),
+                             data = NULL, colour = ifelse(y.val > 0, "green", "red")) +
+                ggtitle(paste(geneName, "shRNA median rank within", object@cancers[cancerIndex])) +
                 xlab("Ranked shRNAs") +
                 ylab("shRNA FC Score") +
                 theme
